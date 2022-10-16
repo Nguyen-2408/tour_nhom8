7035359e4393f7ec86f925f4c8efccd9742e5cf1
Nguyen-2408 <itachihin@gmail.com>
2022-10-16 10:33:21 +0700

    new commit

--- mvc/views/MasterLayout.php
+++ mvc/views/MasterLayout.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
     <link rel="stylesheet" href="<?php echo _WEB_ROOT;?>/public/css/main.css">
-    <link rel="stylesheet" href="<?php echo _WEB_ROOT;?>/public/css/datvemb.css">
     
     <title>Trang chủ</title>
 </head>
@@ -32,7 +31,7 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="<?php echo _WEB_ROOT;?>/public/js/main.js"></script>
-    <script src="<?php echo _WEB_ROOT;?>/public/js/datvemb.js"></script>
+
     
 </body>
 </html>
